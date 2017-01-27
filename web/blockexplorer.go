5ad3151445504f35a557e2a9dcf001be39f82a7b
Tuan Pham Anh <baabeetaa@gmail.com>
2017-01-28 01:31:55 +0700

    pretty layout for /blockexplorer/block

--- web/blockexplorer.go
+++ web/blockexplorer.go
@@ -114,6 +114,95 @@ func BlockExplorer_ConsensusState_Handler(w http.ResponseWriter, req *http.Reque
 	return
 }
 
+/**
+{
+   "jsonrpc":"2.0",
+   "id":"",
+   "result":[
+      3,
+      {
+         "block_meta":{
+            "hash":"DB14E6A3C2E71B09B2D2E71A36E249D220D2FEB0",
+            "header":{
+               "chain_id":"dawn-chain",
+               "height":3276,
+               "time":"2017-01-27T16:00:56.829Z",
+               "num_txs":0,
+               "last_block_id":{
+                  "hash":"000F140AF954A5D04B8D68486BFF0460D88E60B2",
+                  "parts":{
+                     "total":1,
+                     "hash":"DD410CFF60A7081744D7D14339C1566D184FEBCD"
+                  }
+               },
+               "last_commit_hash":"EC5A3209EB22532F12D909B33AB6F4F07F848024",
+               "data_hash":"",
+               "validators_hash":"FEB47FCB45234702C8ABB4C860C772D1B53A310C",
+               "app_hash":"84201BB94504493163B3CDC002EA2C11F604A1BE"
+            },
+            "parts_header":{
+               "total":1,
+               "hash":"083CAFEE9509E2E90C2F73D380F58CEEB67293A1"
+            }
+         },
+         "block":{
+            "header":{
+               "chain_id":"dawn-chain",
+               "height":3276,
+               "time":"2017-01-27T16:00:56.829Z",
+               "num_txs":0,
+               "last_block_id":{
+                  "hash":"000F140AF954A5D04B8D68486BFF0460D88E60B2",
+                  "parts":{
+                     "total":1,
+                     "hash":"DD410CFF60A7081744D7D14339C1566D184FEBCD"
+                  }
+               },
+               "last_commit_hash":"EC5A3209EB22532F12D909B33AB6F4F07F848024",
+               "data_hash":"",
+               "validators_hash":"FEB47FCB45234702C8ABB4C860C772D1B53A310C",
+               "app_hash":"84201BB94504493163B3CDC002EA2C11F604A1BE"
+            },
+            "data":{
+               "txs":[
+
+               ]
+            },
+            "last_commit":{
+               "blockID":{
+                  "hash":"000F140AF954A5D04B8D68486BFF0460D88E60B2",
+                  "parts":{
+                     "total":1,
+                     "hash":"DD410CFF60A7081744D7D14339C1566D184FEBCD"
+                  }
+               },
+               "precommits":[
+                  {
+                     "validator_address":"D0F50DE71B2EBA74290C2F3DBB2AEAA54B289C8A",
+                     "validator_index":0,
+                     "height":3275,
+                     "round":0,
+                     "type":2,
+                     "block_id":{
+                        "hash":"000F140AF954A5D04B8D68486BFF0460D88E60B2",
+                        "parts":{
+                           "total":1,
+                           "hash":"DD410CFF60A7081744D7D14339C1566D184FEBCD"
+                        }
+                     },
+                     "signature":[
+                        1,
+                        "62F6AB8748786C0229E38C4EB437485CB4F75C03F868A5896FB54616BBF295F8DEF0971E5C04EAAB21D1D395516553002FCC6F363423A81A84FA0DC954A57802"
+                     ]
+                  }
+               ]
+            }
+         }
+      }
+   ],
+   "error":""
+}
+ */
 func BlockExplorer_Block_Handler(w http.ResponseWriter, req *http.Request) {
 	var data = map[string]interface{}{ }
 
@@ -149,6 +238,9 @@ func BlockExplorer_Block_Handler(w http.ResponseWriter, req *http.Request) {
 	render(w, "blockexplorer_block", ActionResult{Status: "success", Message: "ok", Data: data })
 	return
 }
+
+//////////////////////////////////////////////
+// local functions
 
 func getLastBlockHeight() int64  {
 	var data = map[string]interface{}{ }
