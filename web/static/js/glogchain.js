e1068003bc18336ccd11bd2051cd0c590cdfce76
Tuan Pham Anh <baabeetaa@gmail.com>
2017-01-31 09:06:58 +0700

    fix auto parsing md on post editor page

--- web/static/js/glogchain.js
+++ web/static/js/glogchain.js
@@ -7,10 +7,10 @@
 
 
 
-jQuery(document).ready(function(){
-    single_post_md_parse(); // call this before loadTorrent
-    loadTorrent();
-});
+// jQuery(document).ready(function(){
+//     single_post_md_parse(); // call this before loadTorrent
+//     loadTorrent();
+// });
 
 ///////////////////////////////
 // WebTorrent Video
