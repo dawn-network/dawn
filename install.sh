429ab3c3cbdc2a134789d4fcd5768f96e2b90e8f
Jacob Gadikian <faddat@gmail.com>
2017-03-14 19:10:15 +0700

    Update install.sh
    Updated to a version known to work on debian sid.

--- install.sh
+++ install.sh
@@ -1,6 +1,14 @@
-apt install build-essential bison
-bash < <(curl -s -S -L https://raw.githubusercontent.com/moovweb/gvm/master/binscripts/gvm-installer)
-source /home/$USER/.gvm/scripts/gvm
+#/bin/bash
+echo "what is your public IP address?"
+read -p 'Public IP' uservar
+apt install build-essential bison git
+if [[ ! -f ./gvm-installer ]]
+then
+	wget https://raw.githubusercontent.com/moovweb/gvm/master/binscripts/gvm-installer
+fi
+chmod +x gvm-installer
+./gvm-installer
+source /$HOME/.gvm/scripts/gvm
 gvm install go1.8 -B -pb
 gvm use go1.8 --default
 mkdir $GOPATH/bin
@@ -10,6 +18,7 @@ cd $GOPATH/src/github.com/tendermint/tendermint
 git branch develop
 make install
 go get -u github.com/dawn-network/glogchain/...
+cd $GOPATH/src/github.com/dawn-network/glogchain
 git branch develop
 cd $GOPATH/src/github.com/dawn-network/glogchain
 glide install
