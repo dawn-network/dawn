ce83f71b4cd71089a6c680ed92abee17be385f9b
Jacob Gadikian <faddat@gmail.com>
2017-03-14 20:02:46 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -29,4 +29,4 @@ make install
 cd ~/
 ipfs init
 ipfs cat /ipfs/QmVLDAhCY3X9P2uRudKAryuQFPM5zqA3Yij1dY8FpGbL7T/readme
-
+echo "Please ensure that you have set up any needed forwarding.  This script detects your public IP address, but there are many good reasons why your machine may not be using a public IP address.  We've no love for NAT, so if you're running a validator, we do expect that you'll be able to use the forwarding features on your router.
