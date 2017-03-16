082efe7a5ad27325b57f08d00c76316b8b4a0da0
Jacob Gadikian <faddat@gmail.com>
2017-03-16 08:13:51 +0700

    Update and rename installx86.sh to install.sh
    Sorry!  I forgot that the solution was actually to do 1.4 and then do 1.8 :(.

--- install.sh
+++ install.sh
@@ -5,19 +5,14 @@ echo -n "node listening address $PUBIP:46656"
 echo -n "HTTP address [ENTER]: $PUBIP:80"
 apt-get update
 apt-get -y upgrade
-apt-get -y install build-essential bison git curl protobuf-compiler
-git clone https://github.com/hypriot/golang-armbuilds.git golang-armbuilds-1.4
-cd golang-armbuilds-1.4
-export SKIP_TEST=1
-./make-tarball-go1.4.sh
-sudo tar golang-armbuilds-1.4/go*.tar.gz /usr/local/ 
+apt-get -y install  curl git mercurial make binutils bison gcc build-essential protobuf-compiler
 curl -s -S -L https://raw.githubusercontent.com/moovweb/gvm/master/binscripts/gvm-installer| bash
 source /root/.gvm/scripts/gvm
-export GOROOT_BOOTSTRAP=/usr/local/go/
-export GOPATH=$HOME/go 
-mkdir -p $GOPATH/bin
+gvm install go1.4
+gvm use go1.4
 gvm install go1.8 -pb
 gvm use go1.8 --default
+mkdir -p $GOPATH/bin
 go get -u github.com/Masterminds/glide
 go get -u github.com/tendermint/tendermint/...
 cd $GOPATH/src/github.com/tendermint/tendermint
